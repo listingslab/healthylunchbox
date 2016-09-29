25ec98bbf76d4492acc04d958e8d7ee11c296380
Ryan Callaghan <me@ryancallaghan.com.au>
2016-09-29 17:12:19 +1000

    Separating components into component file, using React  Router v4 to serve components.

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default() => {
+  return (
+    <div className="App-header">
+      <img src="img" className="App-logo" alt="logo" />
+      <h2>Healthy eating</h2>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import App from './components/App';
+import Header from './components/Header';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Match exactly pattern="/" component={App} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
