12412bbe237996ad15d7719b37c7a0cd76c118b8
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-01 22:00:57 +1000

    Updated home component, will need to review.

--- client/components/Home/index.js
+++ client/components/Home/index.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import Hero from '../Hero'
+import Card from '../Card'
 
 export default () => {
   return (
-    <h2>this is home</h2>
+    <div>
+      <div className="row hero">
+        <div className="container">
+          <Hero />
+        </div>
+      </div>
+      <div className="row">
+        <div className="container">
+          <Card
+            title="Tips"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut purus dolor. Nam metus sapien, vehicula aliquet interdum sit amet, sodales sed purus. Donec vitae euismod lacus. Etiam porttitor bibendum lorem eu luctus."
+            link="/information/tips"
+            label="More" />
+
+          <Card
+            title="Get Inspired"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut purus dolor. Nam metus sapien, vehicula aliquet interdum sit amet, sodales sed purus. Donec vitae euismod lacus. Etiam porttitor bibendum lorem eu luctus."
+            link="/information/inspiration"
+            label="Start with this"/>
+        </div>
+    </div>
+    </div>
   )
 }
