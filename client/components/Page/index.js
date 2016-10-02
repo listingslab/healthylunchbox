72bfe91730089b9f51a9ecd0eb518da9c181a16e
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-02 22:58:09 +1100

    Getting local storage data or fetching pages data if it does not exist, I will remove this from component at the end of the project.

--- client/components/Page/index.js
+++ client/components/Page/index.js
@@ -12,16 +12,24 @@ export default class Page extends Component {
     }
   }
 
-  componentDidMount() {
-    fetch('http://localhost:8888/wp-json/wp/v2/pages?slug=' + this.props.params.slug)
-      .then(response => response.json())
-      .then(data => this.setState({data: data[0]}))
+  componentWillMount() {
+    const slug = this.props.params.slug
+    if(!sessionStorage.getItem('pages')) {
+      fetch('http://localhost:8888/wp-json/wp/v2/pages')
+        .then(response => response.json())
+        .then(data => sessionStorage.setItem('pages', JSON.stringify(data)))
+    }
+    if(sessionStorage.getItem('pages')) {
+      const allPageData = JSON.parse(sessionStorage.getItem('pages'))
+      const pageData = allPageData.filter(page => page.slug === slug)
+      this.setState({data: pageData[0]})
+    }
   }
 
   render() {
     return (
-      <div>
-        {this.state.data ? <PageContent data={this.state.data}/> : <NotFound /> }
+      <div className="container">
+        { this.state.data ? <PageContent data={this.state.data}/> : <NotFound /> }
       </div>
     )
   }
