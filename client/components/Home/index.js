4bb609f66cc274f8a361a349008de02806bc209b
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-03 17:45:52 +1100

    Content cards now coming from Wordpress and used Request manager

--- client/components/Home/index.js
+++ client/components/Home/index.js
@@ -1,30 +1,45 @@
-import React from 'react'
+import React, { Component } from 'react'
+import fetch from 'isomorphic-fetch'
 import Hero from '../Hero'
 import Card from '../Card'
+import { WP_POSTS } from '../../constants'
+import RequestManager from '../../services/request-manager'
 
-export default () => {
-  return (
-    <div>
-      <div className="row hero">
-        <div className="container">
-          <Hero />
+export default class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      contentCards: []
+    }
+  }
+
+  componentWillMount() {
+    if(!sessionStorage.getItem('contentcards')) {
+      RequestManager.get(WP_POSTS).then(payload => {
+        sessionStorage.setItem('contentcards', JSON.stringify(payload))
+        this.setState({contentCards: payload})
+      })
+    }
+    if(sessionStorage.getItem('contentcards')) {
+      const contentCards = JSON.parse(sessionStorage.getItem('contentcards'))
+      this.setState({contentCards: contentCards})
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row hero">
+          <div className="container">
+            <Hero />
+          </div>
         </div>
+        <div className="row">
+          <div className="container">
+            { this.state.contentCards ? this.state.contentCards.map(cards => <Card key={cards.id} title={cards.title} description={cards.content} link={cards.slug} />) : ""}
+          </div>
       </div>
-      <div className="row">
-        <div className="container">
-          <Card
-            title="Tips"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut purus dolor. Nam metus sapien, vehicula aliquet interdum sit amet, sodales sed purus. Donec vitae euismod lacus. Etiam porttitor bibendum lorem eu luctus."
-            link="/information/tips"
-            label="More" />
-
-          <Card
-            title="Get Inspired"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ut purus dolor. Nam metus sapien, vehicula aliquet interdum sit amet, sodales sed purus. Donec vitae euismod lacus. Etiam porttitor bibendum lorem eu luctus."
-            link="/information/inspiration"
-            label="Start with this"/>
-        </div>
-    </div>
-    </div>
-  )
+      </div>
+    )
+  }
 }
