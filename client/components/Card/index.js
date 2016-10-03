43cb1d66f644729719f804fb3c710220ca19f9c7
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-03 17:46:09 +1100

    Content cards now built through Wordpress API

--- client/components/Card/index.js
+++ client/components/Card/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { Link } from 'react-router'
+import fetch from 'isomorphic-fetch'
+import get from 'lodash.get'
 
 export default class Card extends Component {
   constructor(props) {
@@ -8,12 +10,16 @@ export default class Card extends Component {
   }
 
   render() {
+    const props = this.props
+    const title = get(props, 'title.rendered')
+    const description = get(props, 'description.rendered')
+    const link = '/information/' + this.props.link
+
     return (
       <div className="content-card">
-          <h3>{this.props.title}</h3>
-          {this.props.image ? <img src={this.props.image} alt=""/> : ''}
-          <p>{this.props.description}</p>
-          <Link to={this.props.link}>{this.props.label}</Link>
+          <h3>{title}</h3>
+          <div dangerouslySetInnerHTML={{__html: description}}/>
+          <Link to={link}>Read more</Link>
       </div>
     )
   }
