edf6b741ef87a494ce8de3b5d560f1bd93b6b97e
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-04 16:25:40 +1100

    Removing constructor from Card

--- client/components/Card/index.js
+++ client/components/Card/index.js
@@ -5,10 +5,6 @@ import fetch from 'isomorphic-fetch'
 import get from 'lodash.get'
 
 export default class Card extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     const props = this.props
     const title = get(props, 'title.rendered')
