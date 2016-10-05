a58072063801dbef4e7f5594cb3cc2f2ab1f2dc4
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-06 08:19:31 +1100

    Content cards now custom fields in Wordpress post with category of contentcard

--- client/components/Card/index.js
+++ client/components/Card/index.js
@@ -6,16 +6,20 @@ import get from 'lodash.get'
 
 export default class Card extends Component {
   render() {
+
     const props = this.props
     const title = get(props, 'title.rendered')
     const description = get(props, 'description.rendered')
-    const link = '/information/' + this.props.link
+    const image =  get(props, 'acf.contentcard_image')
+    const label = get(props, 'acf.contentcard_label')
+    const link = '/information/' + get(props, 'acf.contentcard_link')
 
     return (
       <div className="content-card">
           <h3>{title}</h3>
+          { image ? <img src={image} />: "" }
           <div dangerouslySetInnerHTML={{__html: description}}/>
-          <Link to={link}>Read more</Link>
+          <Link to={link}>{label}</Link>
       </div>
     )
   }
