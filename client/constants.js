30047a9744e5b51b4c34479a5a2a478dd8e70cbf
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-06 08:43:53 +1100

    Added tabs and hero to constants, these will be removed when refactored at the end of the project

--- client/constants.js
+++ client/constants.js
@@ -1,3 +1,5 @@
 const URL = 'http://localhost:8888/wp-json/wp/v2/'
 export const WP_PAGES = URL + 'pages'
 export const WP_CONTENTCARDS = URL + 'posts?filter[category_name]=contentcard'
+export const WP_TABS = URL + 'posts?filter[category_name]=infotab'
+export const WP_HERO = URL + 'posts?filter[category_name]=hero'
