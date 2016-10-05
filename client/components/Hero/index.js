96640d7b65c21dedb54f890568f2fc653ad54223
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-06 08:18:55 +1100

    Hero now coming from Wordpress

--- client/components/Hero/index.js
+++ client/components/Hero/index.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { Link } from 'react-router'
-import lunchbox from './lunchbox.png'
+import { WP_HERO } from '../../constants'
+import get from 'lodash.get'
+
 export default class Hero extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      hero: []
+    }
+  }
+
+  componentWillMount() {
+    if(sessionStorage.getItem('hero')) {
+      const hero = JSON.parse(sessionStorage.getItem('hero'))
+      this.setState({hero: hero[0]})
+    }
   }
 
   render() {
+
+    const data = this.state.hero
+    const title = get(data, 'title.rendered')
+    const description = get(data, 'content.rendered')
+    const image =  get(data, 'acf.hero_image')
+    const link = get(data, 'acf.hero_main_link')
+
     return (
       <div className="hero">
-        <Link to="/create-lunch-box">
-          <img src={lunchbox} alt="" />
-        </Link>
+        <div className="hero__content">
+          <h2>{title}</h2>
+          <div dangerouslySetInnerHTML={{__html: description}}/>
+          <Link to={link}>
+            Get Started
+          </Link>
+        </div>
+        <div className="hero__image">
+          <img src={image} />
+        </div>
       </div>
     )
   }
