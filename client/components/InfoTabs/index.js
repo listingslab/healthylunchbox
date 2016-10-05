ae6e9faae5658b8773c73c73f6e95ba6390a7787
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-06 08:21:28 +1100

    Tabs now coming from Wordpress

--- client/components/InfoTabs/index.js
+++ client/components/InfoTabs/index.js
@@ -1,52 +1,55 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import icon_apple from './icon_apple.png'
-import icon_bread from './icon_bread.png'
-import icon_carrot from './icon_carrot.png'
-import icon_cheese from './icon_cheese.png'
-import icon_fish from './icon_fish.png'
-import icon_water from './icon_water.png'
+import { WP_TABS } from '../../constants'
+import get from 'lodash.get'
+import he from 'he'
+import RequestManager from '../../services/request-manager'
 
 Tabs.setUseDefaultStyles(false);
 
 export default class InfoTabs extends Component {
-  handleSelect(index, last) {
+  constructor() {
+    super()
+    this.state = {
+      tabs: []
+    }
   }
+
+  componentWillMount() {
+    const tabs = sessionStorage.getItem('tabs')
+
+    if(!tabs) {
+      RequestManager.get(WP_TABS).then(payload => {
+        console.log(payload)
+        sessionStorage.setItem('tabs', JSON.stringify(payload))
+        this.setState({tabs: payload})
+      })
+    }
+
+    if(tabs) {
+      const parsedTabs = JSON.parse(tabs)
+      this.setState({tabs: parsedTabs})
+    }
+  }
+
+  buildTabs() {
+    return this.state.tabs.map(tabs => <Tab key={tabs.id}><span><img src={get(tabs, 'acf.tab_icon')} /></span></Tab>)
+  }
+
+  buildPanels() {
+    return this.state.tabs.map(tabpanel => <TabPanel key={tabpanel.id}>
+                                              { get(tabpanel, 'acf.tab_image') ? <img src={get(tabpanel, 'acf.tab_image')} /> : "" }
+                                              <h2>{he.decode(get(tabpanel, 'title.rendered'))}</h2>
+                                              <div dangerouslySetInnerHTML={{__html: get(tabpanel, 'content.rendered')}}></div>
+                                            </TabPanel>)
+  }
+
   render() {
     return (
-         <Tabs
-           onSelect={this.handleSelect}
-           selectedIndex={0}
-         >
-          <TabList>
-             <Tab><span><img src={icon_apple}/></span></Tab>
-             <Tab><span><img src={icon_carrot} /></span></Tab>
-             <Tab><span><img src={icon_bread} /></span></Tab>
-             <Tab><span><img src={icon_fish} /></span></Tab>
-             <Tab><span><img src={icon_water} /></span></Tab>
-          </TabList>
-
-           <TabPanel>
-              <h2>Hello from Fruits</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula nunc quis velit auctor, et pharetra risus tincidunt. Fusce fermentum, nulla sed pellentesque eleifend, lorem ipsum pulvinar orci, sed sodales purus felis eu lorem. Etiam ac turpis sed orci hendrerit sollicitudin. Vivamus sodales elit non lacus finibus cursus. Proin eget tortor lacus. Mauris vel nunc aliquam, auctor leo sit amet, tristique neque. Phasellus nec ante volutpat lorem interdum fringilla.</p>
-           </TabPanel>
-           <TabPanel>
-             <h2>Hello from Vegetables & Salads</h2>
-             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula nunc quis velit auctor, et pharetra risus tincidunt. Fusce fermentum, nulla sed pellentesque eleifend, lorem ipsum pulvinar orci, sed sodales purus felis eu lorem. Etiam ac turpis sed orci hendrerit sollicitudin. Vivamus sodales elit non lacus finibus cursus. Proin eget tortor lacus. Mauris vel nunc aliquam, auctor leo sit amet, tristique neque. Phasellus nec ante volutpat lorem interdum fringilla.</p>
-           </TabPanel>
-           <TabPanel>
-             <h2>Hello from Breads & Cereal</h2>
-             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula nunc quis velit auctor, et pharetra risus tincidunt. Fusce fermentum, nulla sed pellentesque eleifend, lorem ipsum pulvinar orci, sed sodales purus felis eu lorem. Etiam ac turpis sed orci hendrerit sollicitudin. Vivamus sodales elit non lacus finibus cursus. Proin eget tortor lacus. Mauris vel nunc aliquam, auctor leo sit amet, tristique neque. Phasellus nec ante volutpat lorem interdum fringilla.</p>
-           </TabPanel>
-           <TabPanel>
-             <h2>Hello from Meat & Dairy</h2>
-             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula nunc quis velit auctor, et pharetra risus tincidunt. Fusce fermentum, nulla sed pellentesque eleifend, lorem ipsum pulvinar orci, sed sodales purus felis eu lorem. Etiam ac turpis sed orci hendrerit sollicitudin. Vivamus sodales elit non lacus finibus cursus. Proin eget tortor lacus. Mauris vel nunc aliquam, auctor leo sit amet, tristique neque. Phasellus nec ante volutpat lorem interdum fringilla.</p>
-           </TabPanel>
-           <TabPanel>
-             <h2>Hello from Water</h2>
-             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vehicula nunc quis velit auctor, et pharetra risus tincidunt. Fusce fermentum, nulla sed pellentesque eleifend, lorem ipsum pulvinar orci, sed sodales purus felis eu lorem. Etiam ac turpis sed orci hendrerit sollicitudin. Vivamus sodales elit non lacus finibus cursus. Proin eget tortor lacus. Mauris vel nunc aliquam, auctor leo sit amet, tristique neque. Phasellus nec ante volutpat lorem interdum fringilla.</p>
-           </TabPanel>
+       <Tabs selectedIndex={0}>
+          <TabList>{this.buildTabs()}</TabList>
+          {this.buildPanels()}
        </Tabs>
     )
   }
