e05b054f79f50c7487336e9b0164d12a3f3f47a2
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-10 12:07:22 +1100

    Updated modal component to check of the current item is in the lunchbox

--- client/components/Modal/index.js
+++ client/components/Modal/index.js
@@ -4,15 +4,20 @@ import Modal from 'react-modal'
 export default class ModalWindow extends Component {
   render() {
     const data = this.props.data
+    const group = data.group
+    const title = data.title
+    const image = data.image
+    const content = data.content
+    const currentItem = sessionStorage.getItem(group)
 
     return (
-      <Modal isOpen={this.props.open}>
-      <button onClick={this.props.state}>Close</button>
+      <Modal isOpen={this.props.open} onRequestClose={this.props.close}>
+        <button onClick={this.props.close}>Close</button>
         <div className="food">
-         <img src={data.image} />
-         <button onClick="">Add to lunchbox</button>
-         <h2>{data.title}</h2>
-         <p>{data.content}</p>
+         <img src={image} />
+         { currentItem === image ? <button onClick={ this.props.remove }>Remove from lunchbox</button> : <button onClick={ this.props.add }>Add to lunchbox</button> }
+         <h2>{title}</h2>
+         <p>{content}</p>
         </div>
      </Modal>
     )
