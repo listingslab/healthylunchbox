faba4cf6b29b6fd4d2d81b95e72ced890ce763b4
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-12 10:52:25 +1100

    Added react-router 2.8.1 framework

--- client/index.js
+++ client/index.js
@@ -1,44 +1,28 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter, Match, Miss } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory, browserHistory } from 'react-router'
 import fetch from 'isomorphic-fetch'
-import Header from './components/Header'
+
+import App from './components/App'
 import Home from './components/Home'
 import Page from './components/Page'
 import Lunchbox from './components/Lunchbox'
 import NotFound from './components/NotFound'
+
 import RequestManager from './services/request-manager'
 import { WP_PAGES, WP_POSTS } from './constants'
 
-import css from  './style.styl';
+import css from  './styles/style.styl';
 
-if(!sessionStorage.getItem('pages')) {
-    RequestManager.get(WP_PAGES).then(payload => {
-      sessionStorage.setItem('pages', JSON.stringify(payload))
-    })
-}
+const Root = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}></IndexRoute>
+      <Route path="/pages/:slug" component={Page}></Route>
+      <Route path="/create-lunch-box" component={Lunchbox}></Route>
+      <Route path="*" component={NotFound}></Route>
+    </Route>
+  </Router>
+)
 
-if(!sessionStorage.getItem('posts')) {
-  RequestManager.get(WP_POSTS).then(payload => {
-    sessionStorage.setItem('posts', JSON.stringify(payload))
-  })
-}
-
-const Root = () => {
-
-  return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <main>
-          <Match exactly pattern="/" component={Home} />
-          <Match pattern="/information/:slug" component={Page} />
-          <Match exactly pattern="/create-lunch-box" component={Lunchbox} />
-          <Miss component={NotFound} />
-        </main>
-      </div>
-    </BrowserRouter>
-  )
-}
-
-render(<Root/>, document.querySelector('#app'));
+render(Root, document.querySelector('#app'));
