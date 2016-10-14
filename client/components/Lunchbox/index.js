135c92d9e1078157974cc58cba622474039380b8
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-14 14:52:28 +1100

    Starting on lunchbox

--- client/components/Lunchbox/index.js
+++ client/components/Lunchbox/index.js
@@ -8,11 +8,23 @@ export default class Lunchbox extends Component {
   render() {
     return (
       <div className="lunchbox">
-        <img src={this.props.breads}/>
-        <img src={this.props.vegies}/>
-        <img src={this.props.fruits}/>
-        <img src={this.props.meatsandalternatives}/>
-        <img src={this.props.dairy}/>
+        <div className="f">
+          <div className="lunchbox__breads">
+            <img src={this.props.breads}/>
+          </div>
+          <div className="lunchbox__vegies">
+            <img src={this.props.vegies}/>
+          </div>
+          <div className="lunchbox__fruits">
+            <img src={this.props.fruits}/>
+          </div>
+          <div className="lunchbox__meatsandalternatives">
+            <img src={this.props.meatsandalternatives}/>
+          </div>
+          <div className="lunchbox__dairy">
+            <img src={this.props.dairy}/>
+          </div>
+        </div>
       </div>
     )
   }
