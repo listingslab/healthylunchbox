af86643a9261b48f206bfc347c1bb98b6a7b590f
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-14 11:53:25 +1100

    Removing classes that don't need to be there.

--- client/components/Modal/index.js
+++ client/components/Modal/index.js
@@ -14,12 +14,10 @@ export default class ModalWindow extends Component {
     return (
       <Modal isOpen={this.props.open} onRequestClose={this.props.close}>
         <button onClick={this.props.close}>Close</button>
-        <div className="food">
          <img src={image} />
          { currentItem === image ? <button onClick={ this.props.remove }>Remove from lunchbox</button> : <button onClick={ this.props.add }>Add to lunchbox</button> }
          <h2>{title}</h2>
          <div dangerouslySetInnerHTML={{__html: content }}></div>
-        </div>
      </Modal>
     )
   }
