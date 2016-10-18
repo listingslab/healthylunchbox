0183eb87d826f88bd4bfff970b8dc1fa7d0fdd60
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-18 13:05:47 +1100

    Pack and unpack! basic styles.

--- client/components/Modal/index.js
+++ client/components/Modal/index.js
@@ -13,11 +13,13 @@ export default class ModalWindow extends Component {
 
     return (
       <Modal isOpen={this.props.open} onRequestClose={this.props.close}>
-        <button onClick={this.props.close}>Close</button>
+        <div className="info">
+        <span className="close" onClick={this.props.close}><b>Close Modal</b></span>
          <img src={image} />
-         { currentItem === image ? <button onClick={ this.props.remove }>Remove from lunchbox</button> : <button onClick={ this.props.add }>Add to lunchbox</button> }
          <h2>{title}</h2>
+         { currentItem === image ? <button className="unpack" onClick={ this.props.remove }>UNPACK IT!</button> : <button className="pack" onClick={ this.props.add }>PACK IT!</button> }
          <div dangerouslySetInnerHTML={{__html: content }}></div>
+         </div>
      </Modal>
     )
   }
