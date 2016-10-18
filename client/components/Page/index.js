c305aec3d34a0bfd99d4e26287ebc541cf3b43a2
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-18 13:05:32 +1100

    Removing request manager

--- client/components/Page/index.js
+++ client/components/Page/index.js
@@ -4,9 +4,6 @@ import { Link } from 'react-router'
 import PageContent from '../Content'
 import NotFound from '../NotFound'
 
-import { WP_PAGES } from '../../constants'
-import RequestManager from '../../services/request-manager'
-
 export default class Page extends Component {
   constructor(props) {
     super(props)
