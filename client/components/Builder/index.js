b0f1b93c36f3af70cd642c27f90884446a34b9bf
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-25 10:36:57 +1100

    Added what is a serve.

--- client/components/Builder/index.js
+++ client/components/Builder/index.js
@@ -134,6 +134,9 @@ export default class Builder extends Component {
               <p>Packed with dietary fibre, vitamins and minerals, wholegrain breads and cereals boost health and fight disease.</p>
               <div className={this.state.hidden.breads ? 'hidden' : ''}>
                 <p>These foods are rich in carbohydrate which is the bodies preferred source of energy – perfect for kids to play and learn all day.</p>
+
+                <h3>What is a serve?</h3>
+                <p>1 slice bread, 1/2 medium roll or flat bread, 1/2 cup cooked rice, pasta or noodles, 3 (35g) crispbreads.</p>
               </div>
               <button onClick={this.handleClick.bind(this, 'breads')}>{this.state.hidden.breads ? 'Read more about this food group' : 'Hide info about this food group'}</button>
             </div>
@@ -152,6 +155,8 @@ export default class Builder extends Component {
                <p>Eating vegies and salads helps maintain a healthy weight and can help to prevent cancer.</p>
                <div className={this.state.hidden.vegies ? 'hidden' : ''}>
                 <p>Vegetables and salads are low in energy and high in fibre, so they help fill you up without providing too many kilojoules. Plus they’re packed full of vital vitamins and minerals, perfect for growing bodies</p> <p>Vegetables are one of the most important features of a healthy diet, however only 6% of adults in NSW are eating enough vegetables and so it’s not surprising that children are not eating enough either. There are heaps of exciting ways to pack vegies and salads in the lunch box. In 13 years of school children eat more than 2,500 school lunches. That’s 2,500 opportunities to east vegetables!</p>
+                <h3>What is a serve?</h3>
+                <p>75g or 1/2 cup of cooked veg or legumes or 1/2 medium potato or 1 cup salad</p>
               </div>
 
                <button onClick={this.handleClick.bind(this, 'vegies')}>{this.state.hidden.vegies ? 'Read more about this food group' : 'Hide info about this food group'}</button>
@@ -170,9 +175,13 @@ export default class Builder extends Component {
              <div>
                <h2>Fruits</h2>
                <p>Eating fruit helps maintain a healthy weight and can help to prevent cancer.</p>
+
                <div className={this.state.hidden.fruits ? 'hidden' : ''}>
                 <p>Fruit is low in energy and high in fibre, so it helps fill you up without providing too many kilojoules. Plus they’re packed full of vital vitamins and minerals.</p>
                 <p>Make fruit a snack in the lunch box every day.</p>
+
+                <h3>What is a serve?</h3>
+                <p>1 medium piece, 2 small pieces, or 1 cup chopped</p>
               </div>
 
               <button onClick={this.handleClick.bind(this, 'fruits')}>{this.state.hidden.fruits ? 'Read more about this food group' : 'Hide info about this food group'}</button>
@@ -195,6 +204,9 @@ export default class Builder extends Component {
                 <p>Lean red meat is high in iron and is an important part of a balanced diet, but eating large quantities of red meat regularly can increase risk of bowel cancer. Cancer Council recommends eating no more than 700g (raw weight) of red meat per week per person.</p>
                 <p>Processed meats such as salami, bacon, and ham are not part of this food group. They are classified as discretionary choices because they are high in saturated fat and/or salt. Eating processed meat can increase risk of bowel and stomach cancer.</p>
                 <p>Cut down on red meat and limit or avoid processed meats to reduce your cancer risk.</p>
+
+                <h3>What is a serve?</h3>
+                <p>65g cooked meat, 2 eggs, 1 small can of tuna, 30g nuts, or 1 cup of legumes</p>
                </div>
 
                <button onClick={this.handleClick.bind(this, 'meatsandalternatives')}>{this.state.hidden.meatsandalternatives ? 'Read more about this food group' : 'Hide info about this food group'}</button>
@@ -218,8 +230,12 @@ export default class Builder extends Component {
               <p>Dairy foods are rich sources of calcium and other minerals, protein, and vitamins including B12.</p>
               <p>The Department of Health recommends reduced-fat varieties for people aged 2 or more, but it’s a personal preference - the most important thing is that children are having calcium rich foods.</p>
               <p>If you choose to avoid dairy products, choose alternative products that have added calcium, such as calcium-enriched soy or rice drinks.</p>
+
+              <h3>What is a serve?</h3>
+              <p>1 cup milk, 2 slices of cheese, 200g tub of yoghurt</p>
+
              </div>
-             
+
              <button onClick={this.handleClick.bind(this, 'dairy')}>{this.state.hidden.dairy ? 'Read more about this food group' : 'Hide info about this food group'}</button>
            </div>
            <div className="s-grid-top s-grid-xs-12 s-grid-sm-6 s-grid-md-4">
