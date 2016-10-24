74029353d0d2fa9ba04ab97635d5a2daa5cf4670
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-24 12:11:00 +1100

    Updated constant to api.healthyluchbox

--- client/constants.js
+++ client/constants.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:8888/wp-json/wp/v2/'
+const URL = 'http://api.healthylunchbox.com.au/wp-json/wp/v2/'
 export const WP_PAGES = URL + 'pages'
 export const WP_POSTS = URL + 'posts?per_page=100'
 export const WP_CONTENTCARDS = URL + 'posts?filter[category_name]=contentcard'
