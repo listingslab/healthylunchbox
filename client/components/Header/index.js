525815c0d18b89bccf5233990c739d17f30eb9b7
Petra Gulicher <petra@ustwo.com>
2016-10-25 12:45:07 +1100

    Add navigation links to the header

--- client/components/Header/index.js
+++ client/components/Header/index.js
@@ -2,13 +2,29 @@ import React from 'react'
 import { Link } from 'react-router'
 import logo from './logo.png';
 
+const navItems = {
+  'Inspiration': 'https://marvelapp.com/14c832e/screen/16163977',
+  'Tips': 'https://marvelapp.com/14c832e/screen/16163976',
+  'About': 'https://marvelapp.com/14c832e/screen/16163975'
+};
+
+const nav = [];
+
+for (var item in navItems) {
+  nav.push(<a href={navItems[item]}>{item}</a>)
+}
+
 export default () => {
     return (
       <header>
         <div className="container">
-          <Link to="/">
-            <img className="logo" src={logo} />
+          <Link className="logo" to="https://marvelapp.com/14c832e/screen/16163973">
+            <img src={logo} alt="Cancer Council NSW Healthy Lunch Box" />
           </Link>
+          <nav>
+            <Link to="/">Lunch Box Builder</Link>
+            {nav}
+          </nav>
         </div>
       </header>
     )
