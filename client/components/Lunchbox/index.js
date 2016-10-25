7b333ecd84c33c48974ea98b6ec8a08b65051423
Ryan Callaghan <me@ryancallaghan.com.au>
2016-10-25 15:44:01 +1100

    Check to see if all items are packed into the lunch box, if they are display a tick!

--- client/components/Lunchbox/index.js
+++ client/components/Lunchbox/index.js
@@ -5,6 +5,7 @@ import fruit from './icon_fruit.svg'
 import dairy from './icon_dairy.svg'
 import breads from './icon_breads.svg'
 import water from './builder_water@2x.png'
+import complete from './ic_plus.png'
 
 export default class Lunchbox extends Component {
   render() {
@@ -26,6 +27,8 @@ export default class Lunchbox extends Component {
           <div className="lunchbox__dairy">
             <img src={this.props.dairy ? this.props.dairy : dairy}/>
           </div>
+
+          <img className="completed" src={this.props.breads && this.props.vegies && this.props.fruits && this.props.meatsandalternatives && this.props.dairy ? complete : "" }/>
         </div>
 
         <img src={water} className="bottle" />
