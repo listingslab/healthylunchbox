4a320b9423ea04e9764117312e5b2729f1ae0d66
listingslab <listingslab@gmail.com>
2017-01-19 15:33:43 +1100

    Update version to 2.0.3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
         <hr />
         &copy; Cancer Council NSW 2017
         <p><a target="_blank" rel="noopener noreferrer" href="http://api.healthylunchbox.com.au/wp-admin">WordPress CMS</a></p>
-
+        <small>Vs 2.0.3</small>
       </div>
     </div>
   );
